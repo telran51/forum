11ee78af7b32d059bb245de67762d277fff01629
edward <edward@pop-os.localdomain>
2024-02-14 20:49:46 +0200

    Complete post serice

--- src/main/java/telran/java51/post/dao/PostRepository.java
+++ src/main/java/telran/java51/post/dao/PostRepository.java
@@ -1,9 +1,17 @@
 package telran.java51.post.dao;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.springframework.data.repository.CrudRepository;
 
 import telran.java51.post.model.Post;
 
 public interface PostRepository extends CrudRepository<Post, String> {
-
+	Stream<Post> findByAuthorIgnoreCase(String author);
+	
+	Stream<Post> findByTagsInIgnoreCase(List<String> tags);
+	
+	Stream<Post> findByDateCreatedBetween(LocalDate from, LocalDate to);
 }
--- src/main/java/telran/java51/post/service/PostServiceImpl.java
+++ src/main/java/telran/java51/post/service/PostServiceImpl.java
@@ -1,6 +1,8 @@
 package telran.java51.post.service;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import telran.java51.post.dto.NewCommentDto;
 import telran.java51.post.dto.NewPostDto;
 import telran.java51.post.dto.PostDto;
 import telran.java51.post.dto.exceptions.PostNotFoundException;
+import telran.java51.post.model.Comment;
 import telran.java51.post.model.Post;
 
 @Service
@@ -37,44 +40,66 @@ public class PostServiceImpl implements PostService {
 
 	@Override
 	public PostDto removePost(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException());
+		postRepository.delete(post);
+		return modelMapper.map(post, PostDto.class);
 	}
 
 	@Override
 	public PostDto updatePost(String id, NewPostDto newPostDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException());
+		String content = newPostDto.getContent();
+		if (content != null) {
+			post.setContent(content);
+		}
+		String title = newPostDto.getTitle();
+		if (title != null) {
+			post.setTitle(title);
+		}
+		Set<String> tags = newPostDto.getTags();
+		if (tags != null) {
+			tags.forEach(post::addTag);
+		}
+		postRepository.save(post);
+		return modelMapper.map(post, PostDto.class);
 	}
 
 	@Override
 	public PostDto addComment(String id, String author, NewCommentDto newCommentDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Post post = postRepository.findById(id).orElseThrow(() -> new 	PostNotFoundException());
+		Comment comment = new Comment(author, newCommentDto.getMessage());
+		post.addComment(comment);
+		postRepository.save(post);
+		return modelMapper.map(post,  PostDto.class); 
 	}
 
 	@Override
 	public void addLike(String id) {
-		// TODO Auto-generated method stub
+		Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException());
+		post.addLike();
+		postRepository.save(post);
 
 	}
 
 	@Override
 	public Iterable<PostDto> findPostByAuthor(String author) {
-		// TODO Auto-generated method stub
-		return null;
+		return postRepository.findByAuthorIgnoreCase(author)
+				.map(p -> modelMapper.map(p, PostDto.class))
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public Iterable<PostDto> findPostsByTags(List<String> tags) {
-		// TODO Auto-generated method stub
-		return null;
+		return postRepository.findByTagsInIgnoreCase(tags)
+				.map(p -> modelMapper.map(p, PostDto.class))
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public Iterable<PostDto> findPostsByPeriod(DatePeriodDto datePeriodDto) {
-		// TODO Auto-generated method stub
-		return null;
+		return postRepository.findByDateCreatedBetween(datePeriodDto.getDateFrom(), datePeriodDto.getDateTo())
+				.map(p -> modelMapper.map(p, PostDto.class))
+				.collect(Collectors.toList());
 	}
 
 }
